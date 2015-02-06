1e86fe9f42eaf96afa607d49caaf90c43fcf0581
Diego B. Correa <ddumst@gmail.com>
2015-02-06 15:16:11 -0500

    Animate intro

--- js/master.js
+++ js/master.js
@@ -7,21 +7,22 @@ jQuery(document).ready(function() {
 				var word = jQuery(this).text();
 				var letterW = word.charAt(0);
 
+				var word_calc = '<span>' + word + '</span>';
+				jQuery(this).html(word_calc);
+
+				var wordWidth = jQuery(this).find('span:first').width();
+
+				var letter_calc = '<span>' + letterW + '</span>';
+				jQuery(this).html(letter_calc);
+
+				var letterWidth = jQuery(this).find('span:first').width();
+
+				jQuery(this).html(word);
+
 				if(widthOf){
-					var word_calc = '<span>' + word + '</span>';
-					jQuery(this).html(word_calc);
-
-					var wordWidth = jQuery(this).find('span:first').width();
-
 					jQuery(this).css({'width':wordWidth+'px'});
-				}else{
-					var letter_calc = '<span>' + letterW + '</span>';
-					jQuery(this).html(letter_calc);
-
-					var letterWidth = jQuery(this).find('span:first').width();
-
-					jQuery(this).html(word);
-					jQuery(this).css({'width':letterWidth+'px'});
+				}else{					
+					jQuery(this).animate({'width':letterWidth+'px'});
 				}
 				
 			});
